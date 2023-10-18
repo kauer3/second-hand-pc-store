5ea8767054e62176db0bbe2e508c521774d1e33b
Kauê Reis <kaue0607@gmail.com>
2023-10-18 23:41:05 +1300

    Add header and color schemes

--- Public/Layout/header.php
+++ Public/Layout/header.php
@@ -1,8 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="Layout/reset.css">
+    <link rel="stylesheet" href="Layout/theme.css">
+    <link rel="stylesheet" href="Layout/style.css">
 </head>
+
 <body>
+    <header>
+        <div class="menu-container">
+            <div class="menu-button">
+                <div class="menu-icon">&#9776;</div>
+            </div>
+            <div class="menu">
+                <div class="menu-item">Home</div>
+                <div class="menu-item">About</div>
+                <div class="menu-item">Contact</div>
+            </div>
+        </div>
+        <div class="logo-container">
+            <img src="Images/lark-logo.png" alt="LARK">
+        </div>
+        <div class="search-bar">
+            <input type="text" class="search-input" placeholder="Search...">
+            <i class="search-icon fa fa-search"></i>
+        </div>
+        <button class="login-button">Login</button>
+        <div class="cart-icon">&#128722;</div>
+    </header>
