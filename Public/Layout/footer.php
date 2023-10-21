1bfcb2bab1308689db443726ddae01364b860229
Kauê Reis <kaue0607@gmail.com>
2023-10-22 04:13:54 +1300

    Update footer and header styles

--- Public/Layout/footer.php
+++ Public/Layout/footer.php
@@ -1,26 +1,16 @@
 <footer>
     <div class="footer-container">
         <div class="logo-column">
-            <span class="logo text">LARK</span>
+            <span class="logo">LARK</span>
             <span class="trademark text">© 2023</span>
             <span class="trademark text">Privacy — Terms</span>
         </div>
-        <div class="social-media-grid text">
-            <div class="social-media-icon">
+        <div class="social-media-grid">
                 <i class="fa-brands fa-facebook-square"></i>
-            </div>
-            <div class="social-media-icon">
                 <i class="fa-brands fa-twitter-square"></i>
-            </div>
-            <div class="social-media-icon">
                 <i class="fa-brands fa-instagram-square"></i>
-            </div>
-            <div class="social-media-icon">
                 <i class="fa-brands fa-youtube-square"></i>
-            </div>
-            <div class="social-media-icon">
                 <i class="fa-brands fa-linkedin"></i>
-            </div>
         </div>
         <div class="contacts">
             <span class="phone text">99 999 9999</span>
