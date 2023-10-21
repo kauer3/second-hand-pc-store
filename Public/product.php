0ec7cf7c55686c200a01c9f91e05100c76d9a894
Kauê Reis <kaue0607@gmail.com>
2023-10-22 04:15:43 +1300

    Implement product page

---
+++ Public/product.php
@@ -0,0 +1,54 @@
+<?php
+include "Layout/header.php";
+?>
+<main class="product-main">
+    <h1 class="product-title">Name Of The Product</h1>
+    <div class="product-info">
+        <div class="images-grid">
+            <div class="sub-images">
+                <div class="sub-image">
+                    <img src="https://picsum.photos/100" alt="Product Image">
+                </div>
+                <div class="sub-image">
+                    <img src="https://picsum.photos/200/300" alt="Product Image">
+                </div>
+                <div class="sub-image">
+                    <img src="https://picsum.photos/300/200" alt="Product Image">
+                </div>
+                <div class="sub-image">
+                    <img src="https://picsum.photos/400" alt="Product Image">
+                </div>
+            </div>
+            <div class="main-image">
+                <img src="https://picsum.photos/500/900" alt="Product Image">
+            </div>
+        </div>
+        <div class="specs-and-buttons">
+            <h3 class="price">$99.00</h3>
+            <button class="cart-button">Add To Cart</button>
+            <button class="wishlist-button">Add To Wishlist</button>
+            <span class="specs">
+                Specification 1 of the product<br>
+                Specification 2 of the same product<br>
+                Specification 3 of the same product as well<br>
+                Specification 4 of the product<br>
+                Specification 5 of the same product<br>
+                Specification 6 of the same product as well<br>
+                Specification 7 of the product
+            </span>
+        </div>
+    </div>
+    <div class="description">
+        <h3 class="description-title">Description</h3>
+        <p class="description-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non diam phasellus vestibulum lorem sed. Arcu vitae elementum curabitur vitae nunc. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Nisl vel pretium lectus quam id leo in. Sed viverra tellus in hac habitasse. Faucibus vitae aliquet nec ullamcorper sit amet risus nullam. Egestas integer eget aliquet nibh praesent. Elementum integer enim neque volutpat ac tincidunt vitae semper. In massa tempor nec feugiat nisl. Scelerisque fermentum dui faucibus in ornare quam. Urna molestie at elementum eu facilisis sed. Quis blandit turpis cursus in hac habitasse. Lacus vel facilisis volutpat est velit egestas dui. Elit ut aliquam purus sit amet luctus venenatis. Enim tortor at auctor urna nunc id cursus metus aliquam. Tempor orci eu lobortis elementum nibh tellus molestie nunc non.
+
+            Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique. Vitae congue mauris rhoncus aenean vel elit scelerisque. Auctor augue mauris augue neque gravida in. Magna eget est lorem ipsum dolor sit amet consectetur. Tincidunt arcu non sodales neque. Venenatis urna cursus eget nunc scelerisque viverra mauris. Facilisis magna etiam tempor orci. Nulla porttitor massa id neque aliquam. Id volutpat lacus laoreet non curabitur gravida arcu ac tortor. Id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique. Purus sit amet luctus venenatis lectus magna fringilla urna. Ut eu sem integer vitae.
+
+            Massa id neque aliquam vestibulum morbi blandit cursus risus at. Phasellus egestas tellus rutrum tellus. Amet porttitor eget dolor morbi. Nunc faucibus a pellentesque sit amet porttitor. Malesuada nunc vel risus commodo viverra maecenas. Tempor orci dapibus ultrices in iaculis nunc sed augue lacus. Dolor sed viverra ipsum nunc aliquet. Leo a diam sollicitudin tempor id eu nisl nunc mi. Id velit ut tortor pretium viverra. Sit amet dictum sit amet justo.
+    </div>
+</main>
+
+<?php
+include "Layout/footer.php";
+?>
