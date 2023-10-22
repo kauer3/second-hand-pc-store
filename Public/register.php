dcbea27cf11ff48dd473575038ab309be26f182b
Kauê Reis <kaue0607@gmail.com>
2023-10-22 23:48:31 +1300

    Implement register page

--- Public/register.php
+++ Public/register.php
@@ -1,7 +1,33 @@
 <?php
-    include "Layout/header.php";
+include "Layout/header.php";
 ?>
-
+<main class="main-content unset-margin form-container">
+    <h1 class="main-title">Register</h1>
+    <form action="submit">
+        <div class="form-input-wrapper">
+            <div class="form-field">
+                <label for="email">Email</label>
+                <input type="text" id="email" name="email">
+            </div>
+            <div class="form-field">
+                <label for="password">Password</label>
+                <input type="text" id="password" name="password">
+            </div>
+            <div class="form-field">
+                <label for="confirm-password">Confirm Password</label>
+                <input type="text" id="confirm-password" name="confirm-password">
+            </div>
+            <div class="form-field">
+                <label for="phone">Phone</label>
+                <input type="text" id="phone" name="phone">
+            </div>
+        </div>
+        <button type="submit">Register</button>
+    </form>
+    <span class="message">
+        Already have an account? <a>Log in here</a>.
+    </span>
+</main>
 <?php
-    include "Layout/footer.php";
+include "Layout/footer.php";
 ?>
