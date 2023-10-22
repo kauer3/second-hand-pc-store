55356cadc006f3dce2628c9bfadb58700242345d
Kauê Reis <kaue0607@gmail.com>
2023-10-23 11:43:43 +1300

    Add cart page

---
+++ Public/cart.php
@@ -0,0 +1,51 @@
+<?php
+include "Layout/header.php";
+?>
+<main class="main-content">
+    <h1 class="main-title text">Cart</h1>
+    <div class="cart-item-list">
+        <div class="cart-item">
+            <div class="cart-item-image square-image-container">
+                <img src="https://picsum.photos/500/900" alt="LARK Computing">
+            </div>
+            <div class="cart-item-text">
+                <span class="cart-item-title text">Name Of Product 1</span>
+                <span class="cart-item-price">$99.00</span>
+                <div class="cart-item-specs">
+                    <p class="text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <p class="text">
+                        Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique.
+                    </p>
+                    <p class="text">
+                        Non diam phasellus vestibulum lorem sed. Arcu vitae elementum curabitur vitae nunc.
+                    </p>
+                    <p class="text">
+                        Vitae congue mauris rhoncus aenean vel elit scelerisque.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="cart-item">
+            <div class="cart-item-image square-image-container">
+                <img src="https://picsum.photos/500" alt="LARK Computing">
+            </div>
+            <div class="cart-item-text">
+                <span class="cart-item-title text">Name Of Product 2</span>
+                <span class="cart-item-price">$99.00</span>
+                <div class="cart-item-specs">
+                    <p class="text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non diam phasellus vestibulum lorem sed. Arcu vitae elementum curabitur vitae nunc. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Nisl vel pretium lectus quam id leo in. Sed viverra tellus in hac habitasse. Faucibus vitae aliquet nec ullamcorper sit amet risus nullam. Egestas integer eget aliquet nibh praesent.
+                    </p>
+                    <p class="text">
+                        Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique. Vitae congue mauris rhoncus aenean vel elit scelerisque. Auctor augue mauris augue neque gravida in. Magna eget est lorem ipsum dolor sit amet consectetur. Tincidunt arcu non sodales neque. Venenatis urna cursus eget nunc scelerisque viverra mauris.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php
+include "Layout/footer.php";
+?>
