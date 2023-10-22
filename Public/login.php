70b46b5da3e70c52d668c4f9e8372e60c1718fd0
Kauê Reis <kaue0607@gmail.com>
2023-10-23 02:54:19 +1300

    Implement login page

--- Public/login.php
+++ Public/login.php
@@ -1,7 +1,25 @@
 <?php
     include "Layout/header.php";
 ?>
-
+<main class="main-content unset-margin form-container">
+    <h1 class="main-title">Login</h1>
+    <form action="submit">
+        <div class="form-input-wrapper">
+            <div class="form-field">
+                <label for="email">Email</label>
+                <input type="text" id="email" name="email">
+            </div>
+            <div class="form-field">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password">
+            </div>
+        </div>
+        <button type="submit">Login</button>
+    </form>
+    <span class="message">
+        Still don't have an account? <a href="register.php">Create a new one here</a>.
+    </span>
+</main>
 <?php
     include "Layout/footer.php";
 ?>
