720ffe6803b9e384956722d9b721f995c288c2d6
Kauê Reis <kaue0607@gmail.com>
2023-10-22 23:48:19 +1300

    Implement about page

--- Public/about.php
+++ Public/about.php
@@ -1,7 +1,35 @@
 <?php
-    include "Layout/header.php";
+include "Layout/header.php";
 ?>
-
+<main class="main-content">
+    <div class="about-content-and-title">
+        <h1 class="main-title">About Us</h1>
+        <div class="about-content-container">
+            <div class="about-text paragraphs">
+                <p class="text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non diam phasellus vestibulum lorem sed. Arcu vitae elementum curabitur vitae nunc. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Nisl vel pretium lectus quam id leo in. Sed viverra tellus in hac habitasse. Faucibus vitae aliquet nec ullamcorper sit amet risus nullam. Egestas integer eget aliquet nibh praesent.
+                </p>
+                <p class="text">
+                    Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique. Vitae congue mauris rhoncus aenean vel elit scelerisque. Auctor augue mauris augue neque gravida in. Magna eget est lorem ipsum dolor sit amet consectetur. Tincidunt arcu non sodales neque. Venenatis urna cursus eget nunc scelerisque viverra mauris.
+                </p>
+            </div>
+            <div class="image-and-squares">
+                <div class="about-image square-image-container">
+                    <img src="https://picsum.photos/500/900" alt="About Us Image">
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<div class="quote-container">
+    <span class="quote">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    </span>
+    <div class="author-image square-image-container">
+        <img src="https://picsum.photos/50" alt="Author">
+    </div>
+    <span class="author-name">John Smith, CTO at LARK Computing</span>
+</div>
 <?php
-    include "Layout/footer.php";
+include "Layout/footer.php";
 ?>
