ddba72713db36366d8a32eac4338b1e7bf9c9ca8
Kauê Reis <kaue0607@gmail.com>
2023-10-23 17:58:57 +1300

    Update styles

--- Public/Layout/header.php
+++ Public/Layout/header.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="Layout/cart.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/brands.min.css" integrity="sha512-W/zrbCncQnky/EzL+/AYwTtosvrM+YG/V6piQLSe2HuKS6cmbw89kjYkp3tWFn1dkWV7L1ruvJyKbLz73Vlgfg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-     <script src="scroll.js"></script> 
+    <script src="scroll.js"></script>
 </head>
 
 <body>
@@ -47,5 +47,7 @@
             </button>
         </form>
         <button class="login-button" onclick="document.location.href='login.php'">Login</button>
-        <i class="cart-icon fa fa-cart-shopping"></i>
+        <a href="cart.php">
+            <i class="cart-icon fa fa-cart-shopping"></i>
+        </a>
     </header>
