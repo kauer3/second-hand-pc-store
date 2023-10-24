3f084f1c5fe24930b579b8431a9e1a33946f6548
Aldrone <35581934+Aldrone@users.noreply.github.com>
2023-10-24 17:47:38 +1300

    Auto Gen Product Cards

--- Include/Product.php
+++ Include/Product.php
@@ -8,15 +8,15 @@
         public $desc;
         public $price;
 
-        function __construct()
+        function __construct($prodID,$name,$category,$manufacture,$specs,$desc,$price)
         {
-            $this->prodID;
-            $this->name;
-            $this->category;
-            $this->manufacture;
-            $this->specs;
-            $this->desc;
-            $this->price;
+            $this->prodID = $prodID;
+            $this->name = $name;
+            $this->category = $category;
+            $this->manufacture = $manufacture;
+            $this->specs = $specs;
+            $this->desc = $desc;
+            $this->price = $price;
         }
     }
 ?>
