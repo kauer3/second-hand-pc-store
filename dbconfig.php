34ab60d1f48da9d77f8c387709de3348f227dcaf
Logan Bull <107020194+Torus23@users.noreply.github.com>
2023-11-01 16:19:29 +1300

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,7 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "lark"
+    $servername = "db-php.mysql.database.azure.com";
+    $username = "adminlogan";
+    $password = "Secret123456";
+    $dbname = "lark";
+    $port = 3306;
 ?>
