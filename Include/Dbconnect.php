e57496909225ba44a3fd039a101986911d81e3d5
Logan Bull <107020194+Torus23@users.noreply.github.com>
2023-11-01 16:28:23 +1300

    Update Dbconnect.php

--- Include/Dbconnect.php
+++ Include/Dbconnect.php
@@ -1,7 +1,13 @@
 <?php
     require "../dbconfig.php";
-    $connection = new mysqli($servername, $username, $password, $dbname);
-    if($connection->connect_errno) {
-        die("Connection failed: " . $connection->connect_errno);
-    }
+    $con = mysqli_init();
+    mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+    mysqli_real_connect($con,  $servername, $username, $password, $dbname,  $port, MYSQLI_CLIENT_SSL);
+    if (mysqli_connect_errno($con)) {
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+
+    //$connection = new mysqli($servername, $username, $password, $dbname);
+  //  if($connection->connect_errno) {
+//        die("Connection failed: " . $connection->connect_errno);
+ //   }
 ?>
